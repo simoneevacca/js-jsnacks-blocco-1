c3f5e1e47deb77757e0d044da6c8ae3d02860fc4
simoneevacca <v.simone.vacca@gmail.com>
2024-02-23 12:11:29 +0100

    add snack

---
+++ snack-1/main.js
@@ -0,0 +1,12 @@
+const numb1 = prompt ('inserisci il primo numero');
+const numb2 = prompt ('inserisci il secondo numero');
+
+console.log(numb1, numb2);
+
+
+if (numb1 > numb2){
+    console.log(`il numero più grande è:  ${numb1}`);
+} else {
+    console.log(`il numero più grande è:  ${numb2}`);
+
+}
