73c05eb136d2332d7ebfd4247da3d66c4975247e
simoneevacca <v.simone.vacca@gmail.com>
2024-02-26 12:21:47 +0100

    add whilw

--- snack-6/main.js
+++ snack-6/main.js
@@ -1,11 +1,22 @@
 
 const N = prompt('inserisci il numero');
 let cube = 1;
-for (let i = 0; i < N; i++) {
 
+// for (let i = 0; i < N; i++) {
+
+// let result = cube * cube * cube;
+// console.log(result);
+
+// cube++
+    
+// }
+
+i = 0;
+while (i < N) {
+    
 let result = cube * cube * cube;
 console.log(result);
 
 cube++
-    
+i++
 }
