effe63344ab738b100b937337dd7f892cc157861
simoneevacca <v.simone.vacca@gmail.com>
2024-02-26 11:25:58 +0100

    add snack 3.0

---
+++ snack-3.0/main.js
@@ -0,0 +1,25 @@
+
+
+let sum = 0;
+
+// for (let i = 0; i < 10; i++) {
+//     let element = prompt ('inserisci il numero');
+
+//     let number = parseFloat(element);
+//     sum += number;
+//     console.log(sum);
+    
+// }
+
+i = 0
+
+while (i < 10) {
+    let element = prompt ('inserisci il numero');
+
+    let number = parseFloat(element);
+    sum += number;
+    console.log(sum);
+    i++
+
+}
+
